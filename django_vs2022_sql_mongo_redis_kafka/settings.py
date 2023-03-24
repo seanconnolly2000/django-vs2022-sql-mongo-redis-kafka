2440749eef401cd0046d7334cd7646bc1e52bafe
Sean Connolly <sean@broadworld.com>
2023-03-24 14:54:01 -0700

    Update settings.py

--- django_vs2022_sql_mongo_redis_kafka/settings.py
+++ django_vs2022_sql_mongo_redis_kafka/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '12b4d408-54c4-4868-ae97-db9888aee8a9'
+SECRET_KEY = '00000000-0000-0000-0000-000000000000'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
